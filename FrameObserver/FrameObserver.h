1efa92ff8412935635b28ed119af6f48bc18001a
seanthegoudarzi <76737271+seanthegoudarzi@users.noreply.github.com>
2021-08-22 23:14:52 +0300

    Initial Commit

---
+++ FrameObserver/FrameObserver.h
@@ -0,0 +1,18 @@
+//
+//  FrameObserver.h
+//  FrameObserver
+//
+//  Created by Sean Goudarzi on 8/22/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameObserver.
+FOUNDATION_EXPORT double FrameObserverVersionNumber;
+
+//! Project version string for FrameObserver.
+FOUNDATION_EXPORT const unsigned char FrameObserverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameObserver/PublicHeader.h>
+
+
