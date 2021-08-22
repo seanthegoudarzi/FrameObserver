d2fed427aba7fff0c581c9e4928c051d7bbf8d5b
seanthegoudarzi <76737271+seanthegoudarzi@users.noreply.github.com>
2021-08-22 23:29:43 +0300

    Add `FrameChangeDelegate`

---
+++ FrameObserver/FrameChangeDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  FrameChangeDelegate.swift
+//  FrameObserver
+//
+//  Created by Sean Goudarzi on 8/22/21.
+//
+
+import Foundation
+import UIKit
+
+protocol FrameChangeDelegate: AnyObject {
+
+    func frameChangeDelegateDidChange(for view: UIView, _ frame: CGRect, _ bounds: CGRect)
+
+}
