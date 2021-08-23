321a41ca4cebd30a4c74115efb90f4a91a11291b
seanthegoudarzi <76737271+seanthegoudarzi@users.noreply.github.com>
2021-08-23 11:36:51 +0300

    Add tests for frame change detection

--- FrameObserverTests/FrameObserverTests.swift
+++ FrameObserverTests/FrameObserverTests.swift
@@ -30,4 +30,40 @@ class FrameObserverTests: XCTestCase {
         XCTAssertEqual(viewController.view.hasFrameObserver(), false)
     }
 
+    func testFrame() {
+        viewController.view.setNeedsLayout()
+        viewController.view.layoutIfNeeded()
+        viewController.viewDidAppear(true)
+        let viewFrame = viewController.view.frame
+        let startingWidth = viewFrame.width
+        let startingHeight = viewFrame.height
+
+        let dummyView = UIView()
+        viewController.view.addSubview(dummyView)
+
+        let expectation = self.expectation(description: "dummy view will notify with frame and bounds same as main view")
+
+        dummyView.addFrameObserver { frame, bounds in
+            XCTAssertEqual(startingWidth, frame.width)
+            XCTAssertEqual(startingHeight, frame.height)
+            expectation.fulfill()
+        }
+
+
+        dummyView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: dummyView, attribute: .leading, relatedBy: .equal, toItem: viewController.view, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: dummyView, attribute: .trailing, relatedBy: .equal, toItem: viewController.view, attribute: .trailing, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: dummyView, attribute: .top, relatedBy: .equal, toItem: viewController.view, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: dummyView, attribute: .bottom, relatedBy: .equal, toItem: viewController.view, attribute: .bottom, multiplier: 1, constant: 0),
+        ])
+
+        viewController.view.setNeedsLayout()
+        viewController.view.layoutIfNeeded()
+
+        wait(for: [expectation], timeout: 3)
+
+    }
+
 }
